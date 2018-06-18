54e7e4e98540f420dfd2534dac243d2dc924d81e
fox <fox@mail.altstu.ru>
2018-06-18 13:17:37 +0700

    bash

--- autoformat.py
+++ autoformat.py
@@ -1,15 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import re
-import os
 import sys
-import subprocess
-
-
-BASE_DIR = os.path.dirname(__file__)
-
-rules_pre = []
-rules_post = []
 
 
 def compile_rules(*ruleset):
@@ -23,9 +15,16 @@ def compile_rules(*ruleset):
     return result
 
 
-rules_pre += compile_rules(
+def replace_all(rules, text):
+    result = text.strip()
+    for expr, new_val in rules:
+        result = expr.sub(new_val, result)
+    return result
+
+
+rules_pre = compile_rules(
     (r'\s(нибудь|ка)\b', r'-\1'),
-    (r'\b(как\w{2,3}|кто|что|чем|где|чей|как|кого|куда|чего|чему|общем|когда|почему|вообще|наконец)\s(то)\b', r'\1-\2'),
+    (r'\b(как\w{2,3}|как|кто|что|чем|где|чей|кого|куда|чего|чему|общем|когда|почему|вообще|наконец)\s(то)\b', r'\1-\2'),
     (r'\b(во)\s(первых|вторых)\b', r'\1-\2'),
     (r'\b(из)\s(за|под)\b', r'\1-\2'),
     (r'\b(кое)\s(как|что|чего)\b', r'\1-\2'),
@@ -44,7 +43,7 @@ def fucking_dot(m):
     return '\n— %s%s — %s' % (s1, s2, s3)
     
 
-rules_post += compile_rules(
+rules_post = compile_rules(
     (r'\n—\s(.*?)([\.\?!…])\s—\s(\w)', fucking_dot),
     (r'\?…', '?..'),
     (r'!…', '!..'),
@@ -68,31 +67,15 @@ template = '''\
 (%d символов без пробелов)'''
 
 
-def main(text):
-    text = replace_all(rules_pre, text)
-    text = run_js(text, 'wikificator.js')
-    text = run_js(text, 'node_modules/eyo/bin/cli.js', '--stdin')
-    text = replace_all(rules_post, text)
-
-    count_words = len(re.findall(r'\w+', text))
-    count_ch = len(re.sub(r'\s+', '', text))
-    return template % (text, count_words, count_ch)
-
-
-def replace_all(re_rules, text):
-    result = text.strip()
-    for expr, new_val in re_rules:
-        result = expr.sub(new_val, result)
-    return result
-
-
-def run_js(text, filename, *args):
-    f = os.path.join(BASE_DIR, filename)
-    b = bytes(text.encode('utf-8'))
-    return subprocess.check_output(('js', f, *args), input=b).decode('utf-8')
-
-
 if __name__ == '__main__':
-    text_in = sys.stdin.read()
-    text_out = main(text_in)
-    sys.stdout.write(text_out)
+    text = sys.stdin.read()
+    if sys.argv[1] == '--pre':
+        text = replace_all(rules_pre, text)
+    elif sys.argv[1] == '--post':
+        text = replace_all(rules_post, text)
+        count_words = len(re.findall(r'\w+', text))
+        count_ch = len(re.sub(r'\s+', '', text))
+        text = template % (text, count_words, count_ch)
+    else:
+        raise NotImplementedError
+    sys.stdout.write(text)
